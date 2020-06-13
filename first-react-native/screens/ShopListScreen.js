2f29bfea527e5420436103e517e7b0b8442a4eb6
Sanskriti Pattanayk <sanskriti.pattanayak@gmail.com>
2020-06-13 21:17:29 +0530

    Dummy Value Shop list.

--- first-react-native/screens/ShopListScreen.js
+++ first-react-native/screens/ShopListScreen.js
@@ -5,6 +5,30 @@ import { Component } from 'react';
 import { Image,StyleSheet, Text, View,TouchableOpacity,Alert} from 'react-native';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 //import { Avatar } from "react-native-elements";
+import { SafeAreaView,FlatList} from 'react-native';
+import Constants from 'expo-constants';
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    shop_name: 'Hari Vegetable Mart',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    shop_name: 'Loyal City Market',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    shop_name: 'Top n Town Superstore',
+  },
+];
+
+function Item({ shop_name }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{shop_name}</Text>
+    </View>
+  );
+}
 
 
 export default class ShopListScreen extends Component {
@@ -16,12 +40,6 @@ export default class ShopListScreen extends Component {
 		navigator.geolocation.getCurrentPosition(
 			position => {
         const longitude = JSON.stringify(position.coords.longitude);
-				//const location = JSON.parse(locationstr);
-        //const longitude = locationstr.longitude
-        //alert(location.longitude)
-				//this.setState({ longitude });
-        //const latitude = locationstr.latitude
-        //alert(location.latitiude)
 				this.setState({ longitude });
         const latitude = JSON.stringify(position.coords.latitude)
         this.setState({latitude});
@@ -38,10 +56,17 @@ render()
       <View style={styles.welcomeContainer}>
       <TouchableOpacity onPress={this.findCoordinates}>
 					<Text style={styles.welcome}>Find My Coords?</Text>
-					<Text>Longitude: {this.state.longitude}</Text>
-          <Text>Latitude: {this.state.latitude}</Text>
+					<Text style={styles.welcome}>Longitude: {this.state.longitude}</Text>
+          <Text style={styles.welcome}>Latitude: {this.state.latitude}</Text>
 				</TouchableOpacity>
     </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item shop_name={item.shop_name} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
     </ScrollView>
   );
 }
@@ -58,6 +83,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fafafa',
+    marginTop: Constants.statusBarHeight,
+  },
+  welcome: {
+    fontSize:18
   },
   contentContainer: {
     paddingTop: 15,
@@ -73,5 +102,15 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     marginTop: 3,
     marginLeft: -10,
+  },
+  item: {
+    backgroundColor: '#000000',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
+    color:'#C0C0C0'
   },
 });
